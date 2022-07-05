277bea7d7c9953573a2e9ec6ef5cea37a5a53da0
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-05 18:35:28 +0300

    linters

--- src/__test__/Quote.test.js
+++ src/__test__/Quote.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from 'react-test-renderer';
-import Quote from '../components/Quote';
+import Quote from "../components/Quote";
 import '@testing-library/jest-dom';
 
 describe('Test of Quote page', () => {
--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -1,4 +1,4 @@
-import calculate from '../logic/calculate';
+import calculate from "../logic/calculate";
 
 describe('Unit tests of calculate.js', () => {
   test('testing 1st operand', () => {
