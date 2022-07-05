e94378829626a9a2dd358852d7bf31c37c3f0e9d
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-05 11:54:41 +0300

    Calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,49 +1,187 @@
-import './calculator.css';
-import React, { useState } from 'react';
-import calculate from '../logic/calculate';
+import React, { useState } from "react";
+import calculate from "../logic/calculate";
+import Header from './Header';
 
-function Calculator() {
-  const [state, setState] = useState(0);
-  const handleClick = (event) => {
-    setState((state) => calculate(state, event.target.textContent));
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+
+  const updateObject = (e) => {
+    const myObj = calculate(state, e.target.textContent);
+    setState(myObj);
   };
 
+  const { next, operation, total } = state;
+  const op = operation === "%" ? "mod" : operation;
+  let result = "";
+  if (total) {
+    result = `${total} ${op || ""} ${next || ""}`;
+  } else if (next) {
+    result = `${next} ${op || ""}`;
+  }
+
   return (
+    <>
+      <Header />
+      <section className="calculator-section">
+        <h2>Let's do some Maths!</h2>
     <div className="container">
-      <div className="calculator">
-        <div className="row1 result">{state.next || state.total || 0}</div>
-        <div className="row2">
-          <button type="button" className="btn btn-gray" onClick={handleClick}>AC</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>+/-</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>%</button>
-          <button type="button" className="btn btn-orange" onClick={handleClick}>÷</button>
+      <div className="input-field">
+        <p>{result || 0}</p>
+      </div>
+      <div className="wrapper">
+        <div className="row">
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            %
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button operator-button"
+          >
+            ÷
+          </button>
         </div>
-        <div className="row3">
-          <button type="button" className="btn btn-gray" onClick={handleClick}>7</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>8</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>9</button>
-          <button type="button" className="btn btn-orange" onClick={handleClick}>x</button>
+        <div className="row">
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            7
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            8
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            9
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button operator-button"
+          >
+            x
+          </button>
         </div>
-        <div className="row4">
-          <button type="button" className="btn btn-gray" onClick={handleClick}>4</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>5</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>6</button>
-          <button type="button" className="btn btn-orange" onClick={handleClick}>-</button>
+        <div className="row">
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            4
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            5
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            6
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button operator-button"
+          >
+            -
+          </button>
         </div>
-        <div className="row5">
-          <button type="button" className="btn btn-gray" onClick={handleClick}>1</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>2</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>3</button>
-          <button type="button" className="btn btn-orange" onClick={handleClick}>+</button>
+        <div className="row">
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            1
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            2
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            3
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button operator-button"
+          >
+            +
+          </button>
         </div>
-        <div className="row6">
-          <button type="button" className="btn btn0 btn-gray" onClick={handleClick}>0</button>
-          <button type="button" className="btn btn-gray" onClick={handleClick}>.</button>
-          <button type="button" className="btn btn-orange" onClick={handleClick}>=</button>
+        <div className="row">
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-large-button"
+          >
+            0
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button"
+          >
+            .
+          </button>
+          <button
+            type="button"
+            onClick={updateObject}
+            className="clear-small-button operator-button"
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
+      </section>
+    </>
   );
-}
-
+};
 export default Calculator;
